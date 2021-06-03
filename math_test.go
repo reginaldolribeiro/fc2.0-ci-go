90dbeaa26f0f62a1e98f937ed002c35e9d414f5e
Reginaldo Ribeiro <reginaldolribeiro@gmail.com>
2021-06-03 20:59:36 -0300

    fix bug soma method name

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(1, 1)
+	total := soma(1, 1)
 
 	if total != 2 {
 		t.Errorf("Resultado da soma eh invalido: Resultado %d. Esperado %d", total, 2)
